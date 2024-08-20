9285219936b15fde14867b7c67986952d9e93017
Sébastien VIDAL <svidal@chapsvision.com>
2024-08-20 17:17:53 +0200

    improve packages, rebuild

--- lib/Icon.d.ts
+++ lib/Icon.d.ts
@@ -1,6 +1,6 @@
 import * as React from "react";
 import type { iPropsNode, tVariant, tSize } from "./types";
-export type tICon = "android" | "apple" | "linux" | "windows" | "barcode" | "fingerprint" | "nfc" | "battery-empty" | "battery-quarter" | "battery-half" | "battery-three-quarters" | "battery-full" | "money-bill" | "credit-card" | "play" | "pause" | "stop" | "amazon" | "angular" | "asterisk" | "ban" | "check" | "circle" | "cog" | "edit" | "eye" | "file-invoice" | "gamepad" | "google" | "headset" | "hdd" | "lightbulb" | "lock" | "microchip" | "plug" | "plus" | "power" | "print" | "question" | "react" | "save" | "sync" | "times" | "trash" | "tv" | "unlock" | "usb" | "user" | "volume-up" | "wifi";
+export type tICon = "amazon" | "angular" | "android" | "apple" | "google" | "linux" | "windows" | "barcode" | "fingerprint" | "nfc" | "battery-empty" | "battery-quarter" | "battery-half" | "battery-three-quarters" | "battery-full" | "money-bill" | "credit-card" | "mask-face" | "play" | "pause" | "stop" | "asterisk" | "ban" | "check" | "circle" | "cog" | "edit" | "eye" | "file-invoice" | "gamepad" | "headset" | "hdd" | "lightbulb" | "lock" | "microchip" | "plug" | "plus" | "power" | "print" | "question" | "react" | "save" | "sync" | "times" | "toggle-on" | "toggle-off" | "trash" | "tv" | "unlock" | "usb" | "user" | "volume-up" | "wifi";
 export interface iPropsIcon extends iPropsNode {
     "type": tICon;
     "variant"?: tVariant;
