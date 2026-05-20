7b284261c7633b6e2ccdfebdfea3f34e03fe47f0
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-05-21 01:04:07 +0200

    add 'justify' flag to ListItemHeader

--- public/dist/list/ListItemHeader.d.ts
+++ public/dist/list/ListItemHeader.d.ts
@@ -1,6 +1,10 @@
 import * as React from "react";
 import type { iPropsNode } from "../types";
-export default class ListItemHeader extends React.PureComponent<iPropsNode> {
+interface iPropsListItemHeader extends iPropsNode {
+    "justify"?: boolean;
+}
+export default class ListItemHeader extends React.PureComponent<iPropsListItemHeader> {
     static displayName: string;
     render(): React.JSX.Element;
 }
+export {};
